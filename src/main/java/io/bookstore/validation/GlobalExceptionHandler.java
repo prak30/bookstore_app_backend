ba63edbb39bb56e2aac0d9953f614b2dab3ff337
pranav <katkarpranav4@gmail.com>
2022-08-26 05:26:24 +0530

    global exception

---
+++ src/main/java/io/bookstore/validation/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package io.bookstore.validation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import io.bookstore.utility.Response;
+import io.bookstore.utility.UsersException;
+
+	@ControllerAdvice
+	public class GlobalExceptionHandler  {
+		@ExceptionHandler(value=UsersException.class)
+		public ResponseEntity<Response> exceptionHandler(UsersException usersException){
+			Response res = new Response(usersException.getMessage(), 400, null);
+			return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+		}
+}
